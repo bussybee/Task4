f6abbeb83d5466ea3e0267e951654af72ec0c250
bussybee <90615506+bussybee@users.noreply.github.com>
2021-10-18 21:33:54 +0300

    Add files via upload

---
+++ Task4/src/ru/vsu/cs/maslovaei/Main.java
@@ -0,0 +1,31 @@
+package ru.vsu.cs.maslovaei;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double n = readNum("Input n: ");
+        writeNum(n);
+    }
+
+    private static double countSumma(double n) {
+
+        double summa = 0;
+        for (double i = 1, j = n; i <= n && j >= 1; i++, j--) {
+            summa += Math.pow(i, j);
+        }
+        return summa;
+    }
+
+    private static void writeNum(double n) {
+        System.out.println("Result: " + countSumma(n));
+    }
+
+    private static double readNum(String text) {
+        System.out.print(text);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+}
